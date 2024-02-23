6e0b46dac6c2375d69b9e61dab814da5a7fe0de7
dwijverma <dwijverma2002@gmail.com>
2024-02-23 06:31:17 -0500

    mess

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,11 @@
+let length;
+let width;
+
+const calculateArea = () => {
+length = parseFloat(document.getElementById("length").value);
+width = parseFloat(document.getElementById("width").value);
+
+let area = length * area;
+document.getElementById("result").innerText = `The area of rectangle is: ${area}`;
+
+} 
